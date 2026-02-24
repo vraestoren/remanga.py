3ba53d777aa00c86c0dff535ac103d19c8c12560
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-24 12:01:10 +0300

    Update remanga.py

--- src/remanga.py
+++ src/remanga.py
@@ -1,4 +1,4 @@
-import requests
+from requests import Session
 
 class Remanga:
     def __init__(self) -> None:
@@ -6,7 +6,7 @@ class Remanga:
         self.recaptcha_api = "https://www.google.com/recaptcha/api2"
         self.user_id = None
         self.access_token = None
-        self.session = requests.Session()
+        self.session = Session()
         self.session.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
         }
